c03bab654a36abeec4b67c493b381de21af01265
jhosuapp <jhosuapvll@gmail.com>
2022-12-16 16:33:34 -0500

    lateral menu

--- public_html/theme/150theme/index.php
+++ public_html/theme/150theme/index.php
@@ -1,4 +1,5 @@
-<?php include './template-part/header.php' ?>
+<?php include './template-part/header-two.php' ?>
 
+<?php include './template-part/menu-lateral.php'?>
 
 <?php include './template-part/footer.php' ?>
---
+++ public_html/theme/150theme/template-part/menu-lateral.php
@@ -0,0 +1,72 @@
+<aside class="lateralMenu">
+    <div class="lateralMenu__container">
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/user-white.svg" alt="assists">
+            </div>
+            <p>Iniciar sesión</p>
+        </a>
+        <a href="#" class="lateralMenu__item active">
+            <div>
+                <img src="./assets/menu/plane-white.svg" alt="assists">
+            </div>
+            <p>Vuelos</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/bed-white.svg" alt="assists">
+            </div>
+            <p>Hoteles</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/car-white.svg" alt="assists">
+            </div>
+            <p>Autos</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/vip-white.svg" alt="assists">
+            </div>
+            <p>Salas VIP</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/tour-white.svg" alt="assists">
+            </div>
+            <p>Turisemana</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/espec-white.svg" alt="assists">
+            </div>
+            <p>Espectáculos</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/play-white.svg" alt="assists">
+            </div>
+            <p>Parques</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/transport-white.svg" alt="assists">
+            </div>
+            <p>Buses</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/assist-white.svg" alt="assists">
+            </div>
+            <p>Assist card</p>
+        </a>
+        <a href="#" class="lateralMenu__item">
+            <div>
+                <img src="./assets/menu/shop-white.svg" alt="assists">
+            </div>
+            <p>Carrito de compras</p>
+        </a>
+    </div>
+
+    <div class="lateralMenu__background"></div>
+</aside>
