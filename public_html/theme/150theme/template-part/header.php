aec02cf77cd6a11e99e3dfc6e1ecef1338363419
abnervargas <abner.rivera@150porciento.com>
2022-12-19 09:57:53 -0500

    solve preloader error

--- public_html/theme/150theme/template-part/header.php
+++ public_html/theme/150theme/template-part/header.php
@@ -8,8 +8,12 @@
     <link rel="stylesheet" href="../150theme/css/main.css">
 </head>
 <body>
-
-<header class="header" style="background-image: url('../150theme/assets/menu/bg-header.jpg')">
+    <div id="loadScreen" class="preloader">
+        <span class="loader">
+            <img src="./assets/logo.svg" alt="">
+        </span>
+    </div>
+    <header class="header" style="background-image: url('../150theme/assets/menu/bg-header.jpg')">
     <div class="ctn">
         <a href="#" class="header-logo">
             <img src="../150theme/assets/menu/antojese.svg" alt="antojese">
