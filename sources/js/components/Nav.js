980528e01b3c8e8ba147165996dd2e786845dae1
jhosuapp <jhosuapvll@gmail.com>
2022-12-19 16:38:51 -0500

    config library + base AOS + component for input && header for home

--- sources/js/components/Nav.js
+++ sources/js/components/Nav.js
@@ -1,3 +1,5 @@
+// import { create } from "core-js/core/object";
+
 const Nav = function(){
 
     const Hamburger = ()=>{
@@ -14,22 +16,71 @@ const Nav = function(){
         });
     }
 
+    //
     const ValidationHome = ()=>{
         const getContainerValHome = document.querySelector('.enable-home');
         const getBody = document.querySelector('body');
         getContainerValHome ? getBody.classList.add('enable') : false;
     }
 
+    //ANIMACION SCROLL
+    const ScrollHeaderMobile = ()=>{
+        const getHeader = document.querySelector('.header');
+
+        const reUseValidationScroll = () =>{
+            window.scrollY > 10 ?  getHeader.classList.add('scroll') : getHeader.classList.remove('scroll');
+        }
+        reUseValidationScroll();
+
+        window.addEventListener('scroll', ()=>{
+            reUseValidationScroll();
+        });
+    }
+
+    const OptionsMenuLateral = ()=>{
+        const getAlloptionsOfMenu = document.querySelectorAll('.lateralMenu__container .lateralMenu__item');
+        const dinamicOptions = document.querySelector('.header-options');
+        
+        getAlloptionsOfMenu.forEach((data)=>{
+
+            const getTextContent = data.lastElementChild.textContent;
+            const getSrcImage = data.firstElementChild.firstElementChild.src;
+            const createDiv = document.createElement('div');
+            createDiv.classList.add('header-options__item');
+            const template = `
+                <div>
+                    <img src="${getSrcImage}">
+                </div>
+                <p>${ getTextContent }</p>
+            `;
+            createDiv.innerHTML = template;
+            dinamicOptions.append(createDiv);
+
+            data.addEventListener('click', ()=>{
+                getAlloptionsOfMenu.forEach((data)=>{
+                    data.classList.remove('active');
+                });
+                data.classList.add('active');
+            });
+            
+        });
+        
+    }
+
     return {
         getChildFunctions : function(){
             try {
                 Hamburger();
-            } catch (error) {
-                console.log(error);
-            }
+            } catch (error) { }
             try{
                 ValidationHome();
             }catch(error){ }
+            try{
+                ScrollHeaderMobile();
+            }catch(error){  }
+            try{
+                OptionsMenuLateral();
+            }catch(error){ } 
         }
     }
 }();
--- sources/js/front.js
+++ sources/js/front.js
@@ -2,11 +2,13 @@ import { getChildsNav } from './components/Nav';
 import { getChildsHeader } from './components/Header';
 import { Footer } from './components/Footer';
 import { getChildsSwiper } from './components/Swiper';
-
+import { getChildsAos } from './components/Aos';
+ 
 window.addEventListener('load', ()=>{
     getChildsNav();
     getChildsHeader();
     getChildsSwiper();
+    getChildsAos();
 });
 
 window.addEventListener('DOMContentLoaded', ()=>{
