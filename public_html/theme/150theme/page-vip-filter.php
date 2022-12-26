571a2ac49aa7467d2dc30c45ca75bd54a5c35537
abnervargas <abner.rivera@150porciento.com>
2022-12-26 08:48:26 -0500

    solve bug acordeon

--- public_html/theme/150theme/page-vip-filter.php
+++ public_html/theme/150theme/page-vip-filter.php
@@ -93,87 +93,88 @@
                       <img src="./assets/icons/arrowDown.svg" alt="" />
                     </button>
                   </div>
-                </div>
-                <div class="data hideData ctm-block">
-                  <div class="data-desc">
-                    <h6><strong>Descripción:</strong></h6>
-                    <br />
-                    <h6>
-                      A sanctuary past security control, guests will fly fully
-                      restored from the stresses of traveling to the airport
-                      after a visit to this lounge. Here you can enjoy peace and
-                      quiet away from the noisy departure hall. Guests can
-                      unwind and relax taking advantage of light refreshments,
-                      drinks and newspapers or for those who wish to stay
-                      connected use the Wi-Fi to create your very own workspace
-                      at a lounge.
-                    </h6>
-                    <div class="data-desc-btn">
-                      <button class="btn-s">Traducir</button>
-                    </div>
-                  </div>
-                  <div class="data-service">
-                    <div class="data-service__item">
-                      <h6><strong>Servicios</strong></h6>
-                      <br />
-                      <ul>
-                        <li><span class="li-item"></span>Air Conditioning</li>
-                        <li>
-                          <span class="li-item"></span>Refreshments (Alcoholic)
-                        </li>
-                        <li>
-                          <span class="li-item"></span>Conference Facilities
-                        </li>
-                        <li><span class="li-item"></span>Disabled Access</li>
-                        <li><span class="li-item"></span>Fax</li>
-                        <li>
-                          <span class="li-item"></span>Flight Information
-                          Monitor
-                        </li>
-                        <li><span class="li-item"></span>Internet</li>
-                        <li>
-                          <span class="li-item"></span>Newspapers/Magazines
-                        </li>
-                        <li><span class="li-item"></span>No Smoking</li>
-                        <li>
-                          <span class="li-item"></span>Refreshments (Soft
-                          Drinks)
-                        </li>
-                        <li><span class="li-item"></span>Shower Facilities</li>
-                        <li><span class="li-item"></span>Televisión</li>
-                        <li><span class="li-item"></span>Wi-fi</li>
-                      </ul>
-                    </div>
-
-                    <div class="data-service__item">
-                      <h6><strong>Ubicación</strong></h6>
+                  <div class="data hideData ctm-block">
+                    <div class="data-desc">
+                      <h6><strong>Descripción:</strong></h6>
                       <br />
                       <h6>
-                        Airside - after Security Checks, follow signs for
-                        ‘Airport Lounges’ and the lounge is located on Level 3
-                        above Gate 10. International flights only. Welcome to
-                        The House, where you can eat, drink and relax before you
-                        fly. With delicious seasonal menus, self-serve dishes
-                        and a fully tended bar serving sparkling wines, craft
-                        beers, premium spirits, plus barista-made coffees.
+                        A sanctuary past security control, guests will fly fully
+                        restored from the stresses of traveling to the airport
+                        after a visit to this lounge. Here you can enjoy peace and
+                        quiet away from the noisy departure hall. Guests can
+                        unwind and relax taking advantage of light refreshments,
+                        drinks and newspapers or for those who wish to stay
+                        connected use the Wi-Fi to create your very own workspace
+                        at a lounge.
                       </h6>
+                      <div class="data-desc-btn">
+                        <button class="btn-s">Traducir</button>
+                      </div>
                     </div>
+                    <div class="data-service">
+                      <div class="data-service__item">
+                        <h6><strong>Servicios</strong></h6>
+                        <br />
+                        <ul>
+                          <li><span class="li-item"></span>Air Conditioning</li>
+                          <li>
+                            <span class="li-item"></span>Refreshments (Alcoholic)
+                          </li>
+                          <li>
+                            <span class="li-item"></span>Conference Facilities
+                          </li>
+                          <li><span class="li-item"></span>Disabled Access</li>
+                          <li><span class="li-item"></span>Fax</li>
+                          <li>
+                            <span class="li-item"></span>Flight Information
+                            Monitor
+                          </li>
+                          <li><span class="li-item"></span>Internet</li>
+                          <li>
+                            <span class="li-item"></span>Newspapers/Magazines
+                          </li>
+                          <li><span class="li-item"></span>No Smoking</li>
+                          <li>
+                            <span class="li-item"></span>Refreshments (Soft
+                            Drinks)
+                          </li>
+                          <li><span class="li-item"></span>Shower Facilities</li>
+                          <li><span class="li-item"></span>Televisión</li>
+                          <li><span class="li-item"></span>Wi-fi</li>
+                        </ul>
+                      </div>
 
-                    <div class="data-service__item">
-                      <h6><strong>Horario de apertura</strong></h6>
-                      <br />
-                      <ul>
-                        <li>12:00 - 01:00 Domingo</li>
-                        <li>08:30 - 01:00 Lunes</li>
-                        <li>14:00 - 22:30 Miércoles</li>
-                        <li>08:30 - 16:00 Jueves</li>
-                        <li>12:00 - 01:00 Viernes</li>
-                        <li>08:30 - 01:00 Martes</li>
-                        <li>08:30 - 16:00 Sábado</li>
-                      </ul>
+                      <div class="data-service__item">
+                        <h6><strong>Ubicación</strong></h6>
+                        <br />
+                        <h6>
+                          Airside - after Security Checks, follow signs for
+                          ‘Airport Lounges’ and the lounge is located on Level 3
+                          above Gate 10. International flights only. Welcome to
+                          The House, where you can eat, drink and relax before you
+                          fly. With delicious seasonal menus, self-serve dishes
+                          and a fully tended bar serving sparkling wines, craft
+                          beers, premium spirits, plus barista-made coffees.
+                        </h6>
+                      </div>
+
+                      <div class="data-service__item">
+                        <h6><strong>Horario de apertura</strong></h6>
+                        <br />
+                        <ul>
+                          <li>12:00 - 01:00 Domingo</li>
+                          <li>08:30 - 01:00 Lunes</li>
+                          <li>14:00 - 22:30 Miércoles</li>
+                          <li>08:30 - 16:00 Jueves</li>
+                          <li>12:00 - 01:00 Viernes</li>
+                          <li>08:30 - 01:00 Martes</li>
+                          <li>08:30 - 16:00 Sábado</li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
+
               </div>
             </div>
 
